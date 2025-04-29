86fb7c7da455850953bda11ad4d3c7580117b977
Saad Ahmed Khan <saad.ahmed@10pearls.com>
2025-04-29 14:26:18 +0500

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,6 +6,7 @@ describe('User Authentication Tests', () => {
   var tokenn-1; // Unused variable, token is declared but never used (Issue)
   let token3;;;
   var other;;
+  var other;;
   
   before(() => {
     cy.visit(baseUrl);
